d11ecac910af22d50da855b6d0a22852354a401c
mira-uc3m <100589386@alumnos.uc3m.es>
2026-04-10 22:08:09 +0200

    validate_starting_date mysterious name changes

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -67,15 +67,15 @@ class EnterpriseManager:
 
         return True
 
-    def validate_starting_date(self, t_d):
-        """validates the  date format  using regex"""
-        mr = re.compile(r"^(([0-2]\d|3[0-1])\/(0\d|1[0-2])\/\d\d\d\d)$")
-        res = mr.fullmatch(t_d)
-        if not res:
+    def validate_starting_date(self, date_to_validate):
+        """validates the date format using regex"""
+        date_pattern = re.compile(r"^(([0-2]\d|3[0-1])\/(0\d|1[0-2])\/\d\d\d\d)$")
+        pattern_match = date_pattern.fullmatch(date_to_validate)
+        if not pattern_match:
             raise EnterpriseManagementException("Invalid date format")
 
         try:
-            my_date = datetime.strptime(t_d, "%d/%m/%Y").date()
+            my_date = datetime.strptime(date_to_validate, "%d/%m/%Y").date()
         except ValueError as ex:
             raise EnterpriseManagementException("Invalid date format") from ex
 
@@ -84,7 +84,8 @@ class EnterpriseManager:
 
         if my_date.year < 2025 or my_date.year > 2050:
             raise EnterpriseManagementException("Invalid date format")
-        return t_d
+        return date_to_validate
+
     #pylint: disable=too-many-arguments, too-many-positional-arguments
     def register_project(self,
                          company_cif: str,
